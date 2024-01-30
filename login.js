367e223954bf595a52f964d3024f000ec4af2e7a
Dhikaa <95111513+Aldyyep16@users.noreply.github.com>
2024-01-30 19:20:54 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,19 @@
+function tombolLogin() {
+window.location.href = "index.html" 
+
+
+
+}
+
+function tombolLogin2() {    
+    window.location.href = "admin.html" 
+    
+    
+    
+}
+function tombolLogin3() {    
+    window.location.href = "halaman-guru.html" 
+    
+    
+    
+}
